53e017c562a40baaecfcbbd1b9fe88a2d187bcef
gojo29208 <gojo29208@gmail.com>
2025-05-09 15:28:39 +0300

    feat: update SocialLinks component to use Link for external navigation

--- src/shared/ui/sections/social-links.tsx
+++ src/shared/ui/sections/social-links.tsx
@@ -5,6 +5,7 @@ import Telegram from '@root/public/telegram.svg'
 import { useSectionInView } from '@/shared/hooks/use-section-in-view'
 import Mail from '@root/public/mail.svg'
 import { ExternalLink } from 'lucide-react'
+import Link from 'next/link'
 
 export function SocialLinks() {
 	const { ref } = useSectionInView('Контакти')
@@ -27,18 +28,20 @@ export function SocialLinks() {
 			<div className='w-full p-4'>
 				<div className='grid grid-cols-1 md:grid-cols-2 gap-4 max-w-6xl mx-auto'>
 					{/* Telegram Link */}
-					<div className='bg-zinc-900 rounded-lg p-4 flex items-center justify-between gap-3 lg:min-w-[250px]'>
-						<div className='flex items-center gap-3'>
-							<Telegram />
-						</div>
-						<div className='flex w-full flex-col items-start justify-start text-gray-400 hover:text-white transition-colors'>
-							<div className='text-white text-lg font-bold'>@telegram</div>
-							<div className='flex justify-center items-center w-max text-[12px] cursor-pointer'>
-								<span className='mr-1'>Підписатись</span>
-								<ExternalLink className='w-4 h-4' />
+					<Link href='https://t.me/batman_win_u' target='_blank'>
+						<div className='bg-zinc-900 rounded-lg p-4 flex items-center justify-between gap-3 lg:min-w-[250px]'>
+							<div className='flex items-center gap-3'>
+								<Telegram />
+							</div>
+							<div className='flex w-full flex-col items-start justify-start text-gray-400 hover:text-white transition-colors'>
+								<div className='text-white text-lg font-bold'>@batman_win_u</div>
+								<div className='flex justify-center items-center w-max text-[12px] cursor-pointer'>
+									<span className='mr-1'>Підписатись</span>
+									<ExternalLink className='w-4 h-4' />
+								</div>
 							</div>
 						</div>
-					</div>
+					</Link>
 
 					{/* YouTube Link */}
 					{/* <div className='bg-zinc-900 rounded-lg p-4 flex items-center justify-between gap-3 lg:min-w-[250px]'>
@@ -55,20 +58,22 @@ export function SocialLinks() {
 					</div> */}
 
 					{/* Email Link */}
-					<div className='bg-zinc-900 rounded-lg p-4 flex items-center justify-between gap-3 lg:min-w-[250px]'>
-						<div className='flex items-center gap-3'>
-							<Mail className='w-8 h-8 text-black' />
-						</div>
-						<div className='flex w-full flex-col items-start justify-start text-gray-400 hover:text-white transition-colors'>
-							<div className='text-white text-lg font-bold'>
-								ggmail@tuta.com
+					<Link href='#' target='_blank'>
+						<div className='bg-zinc-900 rounded-lg p-4 flex items-center justify-between gap-3 lg:min-w-[250px]'>
+							<div className='flex items-center gap-3'>
+								<Mail className='w-8 h-8 text-black' />
 							</div>
-							<div className='flex justify-center items-center w-max text-[12px] cursor-pointer'>
-								<span className='mr-1'>Отримати</span>
-								<ExternalLink className='w-4 h-4' />
+							<div className='flex w-full flex-col items-start justify-start text-gray-400 hover:text-white transition-colors'>
+								<div className='text-white text-lg font-bold'>
+									ggmail@tuta.com
+								</div>
+								<div className='flex justify-center items-center w-max text-[12px] cursor-pointer'>
+									<span className='mr-1'>Отримати</span>
+									<ExternalLink className='w-4 h-4' />
+								</div>
 							</div>
 						</div>
-					</div>
+					</Link>
 				</div>
 			</div>
 		</section>
